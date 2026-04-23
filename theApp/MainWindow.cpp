20abd118f832dd3c29a2d45f203f7c51c6ed29bc
loumcnamee <mcnamee.lou@gmail.com>
2026-04-22 23:17:12 -0400

    Minor fixes to example Qt Window app

--- theApp/MainWindow.cpp
+++ theApp/MainWindow.cpp
@@ -1,4 +1,4 @@
-#include "mainwindow.h"
+#include "MainWindow.h"
 
 #include <QtCore/QString>
 #include <QtCore/QFile>
@@ -8,10 +8,9 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
-    m_button = new QPushButton("Store Content", this);
+    
     setupUi(this);
-    //setCentralWidget(m_button);
-    connect(m_button, &QPushButton::clicked, this, &MainWindow::storeContent);
+    
 }
 
 MainWindow::~MainWindow()
@@ -24,6 +23,7 @@ void MainWindow::setupUi(QMainWindow *MainWindow)
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
         MainWindow->resize(800, 600);
+        
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         verticalLayoutWidget = new QWidget(centralwidget);
@@ -32,23 +32,43 @@ void MainWindow::setupUi(QMainWindow *MainWindow)
         verticalLayout = new QVBoxLayout(verticalLayoutWidget);
         verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
         verticalLayout->setContentsMargins(0, 0, 0, 0);
-        pushButton_5 = new QPushButton(verticalLayoutWidget);
-        pushButton_5->setObjectName(QString::fromUtf8("pushButton_5"));
-
-        verticalLayout->addWidget(pushButton_5);
-
-        pushButton_3 = new QPushButton(verticalLayoutWidget);
-        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
-
-        verticalLayout->addWidget(pushButton_3);
-
+        
         pushButton = new QPushButton(verticalLayoutWidget);
         pushButton->setObjectName(QString::fromUtf8("pushButton"));
 
-        verticalLayout->addWidget(pushButton);
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+        pushButton_2->setGeometry(QRect(280, 180, 158, 23));
+        
+        pushButton_3 = new QPushButton(verticalLayoutWidget);
+        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
 
+        pushButton_4 = new QPushButton(centralwidget);
+        pushButton_4->setObjectName(QString::fromUtf8("pushButton_4"));
+        pushButton_4->setGeometry(QRect(320, 80, 81, 81));
+
+
+        pushButton_5 = new QPushButton(verticalLayoutWidget);
+        
+        
         pushButton_6 = new QPushButton(verticalLayoutWidget);
         pushButton_6->setObjectName(QString::fromUtf8("pushButton_6"));
+
+        m_button = new QPushButton("Store Content", verticalLayoutWidget);
+        verticalLayout->addWidget(m_button);
+        //setCentralWidget(m_button);
+        connect(m_button, &QPushButton::clicked, this, &MainWindow::storeContent);
+        
+        verticalLayout->addWidget(pushButton_5);
+
+        
+
+        verticalLayout->addWidget(pushButton_3);
+    
+
+        verticalLayout->addWidget(pushButton);
+
+        
 
         verticalLayout->addWidget(pushButton_6);
 
@@ -56,23 +76,21 @@ void MainWindow::setupUi(QMainWindow *MainWindow)
 
         verticalLayout->addItem(verticalSpacer);
 
-        pushButton_4 = new QPushButton(centralwidget);
-        pushButton_4->setObjectName(QString::fromUtf8("pushButton_4"));
-        pushButton_4->setGeometry(QRect(320, 80, 81, 81));
+        
         QSizePolicy sizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
         sizePolicy.setHorizontalStretch(0);
         sizePolicy.setVerticalStretch(0);
         sizePolicy.setHeightForWidth(pushButton_4->sizePolicy().hasHeightForWidth());
         pushButton_4->setSizePolicy(sizePolicy);
-        pushButton_2 = new QPushButton(centralwidget);
-        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
-        pushButton_2->setGeometry(QRect(280, 180, 158, 23));
+        
+        
         QSizePolicy sizePolicy1(QSizePolicy::Preferred, QSizePolicy::Fixed);
         sizePolicy1.setHorizontalStretch(0);
         sizePolicy1.setVerticalStretch(0);
         sizePolicy1.setHeightForWidth(pushButton_2->sizePolicy().hasHeightForWidth());
         pushButton_2->setSizePolicy(sizePolicy1);
         pushButton_2->setFlat(false);
+        
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
@@ -103,10 +121,12 @@ void MainWindow::storeContent()
  void MainWindow::retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
-        pushButton_5->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
-        pushButton_3->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
+                
+        
+        pushButton_5->setText(QCoreApplication::translate("MainWindow", "PushButton 5", nullptr));
+        pushButton_3->setText(QCoreApplication::translate("MainWindow", "PushButton 3", nullptr));
         pushButton->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
-        pushButton_6->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
-        pushButton_4->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
-        pushButton_2->setText(QCoreApplication::translate("MainWindow", "PushButton", nullptr));
+        pushButton_6->setText(QCoreApplication::translate("MainWindow", "PushButton 6", nullptr));
+        pushButton_4->setText(QCoreApplication::translate("MainWindow", "PushButton 4", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("MainWindow", "PushButton 2", nullptr));
     } // retranslateUi
--- theApp/MainWindow.h
+++ theApp/MainWindow.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QPushbutton>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QSpacerItem>
 #include <QtWidgets/QStatusBar>
