fa51400a5611c329fb98ab4af564ba2eb02da2ca
loumcnamee <mcnamee.lou@gmail.com>
2026-04-28 08:24:44 -0400

    Add interface to set number of lines

--- theApp/BouncingLinesWidget.h
+++ theApp/BouncingLinesWidget.h
@@ -17,6 +17,14 @@ public:
     bool isRunning() const { return m_timer.isActive(); }
     void freeze()    { m_model.zeroVelocity(); }
 
+    void setCount(int n)
+    {
+        m_model.setNumLines(n);
+        if (width() > 0 && height() > 0)
+            m_model.init(width(), height());
+    }
+    int  count() const { return m_model.numLines(); }
+
 protected:
     bool hasHeightForWidth() const override { return true; }
     int  heightForWidth(int w) const override { return w; }
